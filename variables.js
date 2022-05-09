bde4f6dd7276b9a372a27dca383369aad3454389
claudinipaz <claudinipaz@gmail.com>
2022-05-09 12:37:49 -0300

    repo

---
+++ variables.js
@@ -0,0 +1,28 @@
+class Carrito {
+        constructor(color, precio, variedad){
+        let.color=color
+        let.precio=precio
+        let.variedad=variedad
+
+    }
+}
+const Carrito = [];
+
+let carrito1 = new Carrito
+function crearCarrito(){
+
+    let color; (prompt("Ingresa el color del item: "))
+    let precio; (prompt("Ingrese el precio del producto: "))
+    let variedad; (prompt("Ingrese la variedad del item: "))
+    
+
+Carrito==carrito1 (color, precio, variedad);
+Carrito.push(carrito1);
+
+}
+
+
+function guardarCarrito(Carrito) {
+    Carrito.push(carrito1);
+    console.log(carrito1);
+}
